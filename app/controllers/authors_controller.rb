f1fecd0ec2d4f85b89834f08973d3d0caa47f3da
lemanh1997 <lemanh20111997@gmail.com>
2019-08-07 10:25:40 +0700

    edit indent authors cotegories publishers

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,16 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: :show
+
   def new
     @author = Author.new
   end
 
   def show
-    @author = Author.find(params[:id])
   end
 
   def create
     @author = Author.new(author_params)
-    if(@author.save)
+    if @author.save
       flash[:success] = "Create author complete!"
       redirect_to @author
     else
@@ -20,6 +21,10 @@ class AuthorsController < ApplicationController
   private
   def author_params
     params.require(:author).permit(:name, :nickname, :content)
+  end
+
+  def set_author
+    @author = Author.find(params[:id])
   end
 
 end
